6ad2b2087760d5dc3410f2cedec5c5db51ce359a
Orange Cheers <orange_cheers@outlook.com>
2022-04-18 20:51:54 +0800

    DES completed!🎉

--- DESEncryption.cpp
+++ DESEncryption.cpp
@@ -3,32 +3,74 @@
 //
 
 #include "DESEncryption.h"
+#include <iostream>
+std::bitset<48> DESEncryption::keys[16] = {0};
 
-constexpr void DESEncryption::Encrypt64(std::bitset<64> &dst, const std::bitset<64> &src, const std::bitset<64> &key) {
-    generatekeys(key);
+void DESEncryption::Encrypt64(std::bitset<64> &dst, const std::bitset<64> &src, const std::bitset<64> &key, bool isdecryption) {
+    //std::cout << "!" << std::endl;
+    generatekeys(key, isdecryption);
+    std::bitset<64> t;
+    performpermutation(t, src, IP);
+    std::bitset<32> a, b;
+    std::bitset<32> t32;
+    for(int i = 0;i < 32;i++) a[i] = t[i];
+    for(int i = 0;i < 32;i++) b[i] = t[i+32];
+    for(int i = 0;i < 16;i++)
+    {
+        F(t32, b, keys[i]);
+        t32 ^= a;
+        a = b;
+        b = t32;
+    }
+    swap(a, b);
+    for(int i = 0;i < 32;i++) t[i] = a[i];
+    for(int i = 0;i < 32;i++) t[i+32] = b[i];
+    performpermutation(dst, t, IPinv);
+    return ;
 }
 
-void DESEncryption::generatekeys(const std::bitset<64> &key) {
+void DESEncryption::generatekeys(const std::bitset<64> &key, bool isdecryption) {
     std::bitset<28> z, y;
     performpermutation(z, key, PC1z);
     performpermutation(y, key, PC1y);
+    //std::cout << z << "?" << y << std::endl;
     for(int i = 0;i < 16;i++)
     {
-        z = ((z << shift[i]) | (z >> (28 - i)));
-        y = ((y << shift[i]) | (y >> (28 - i)));
+        z = ((z << shift[i]) | (z >> (28 - shift[i])));
+        y = ((y << shift[i]) | (y >> (28 - shift[i])));
         std::bitset<56> tb;
-        for(int j = 0;i < 28;i++) tb[j] = z[j];
-        for(int j = 0;i < 28;i++) tb[j+28] = z[j];
+        for(int j = 0;j < 28;j++) tb[j] = z[j];
+        for(int j = 0;j < 28;j++) tb[j+28] = y[j];
+        //std::cout << tb << "???" << std::endl;
+        //std::cout << i << "??" << std::endl;
         performpermutation(keys[i], tb, PC2);
     }
+    if(isdecryption) std::reverse(keys, keys+16);
     return ;
 }
 
 template<size_t Ndst, size_t Nsrc>
-constexpr void DESEncryption::performpermutation(std::bitset<Ndst> &dst, const std::bitset<Nsrc> &src, const int *permutation) {
+void DESEncryption::performpermutation(std::bitset<Ndst> &dst, const std::bitset<Nsrc> &src,  const int permutation[]) {
     for(int i = 0;i < Ndst;i++)
     {
         dst[i] = src[permutation[i]-1];
     }
+    return ;
+}
+
+void DESEncryption::F(std::bitset<32> &dst, std::bitset<32> &x, std::bitset<48>& key) {
+    std::bitset<48> e;
+    performpermutation(e, x, E);
+    e ^= key;
+    std::bitset<32> t32;
+    for(int i = 0;i < 8;i++)
+    {
+        int h = (e[i] | (e[i+5]<<1));
+        int l = ((e[i+1]) | (e[i+2] << 1) | (e[i+3] << 2) | (e[i+4] << 3));
+        int ind = ((h << 4) | l);
+        for(int j = 0;j < 3;j++)
+            t32[(i << 2) | j] = ((S[i][ind] >> j) & 1);
+    }
+    performpermutation(dst, t32, P);
     return ;
 }
--- main.cpp
+++ main.cpp
@@ -3,19 +3,33 @@
 #include "DESEncryption.h"
 
 int main() {
-//    std::bitset<64> dst, src, key;
-//    std::string s = "HONOKAHO";
-//    for(int i = 0;i < 8;i++)
-//    {
-//        for(int j = 0;j < 8;j++)
-//        {
-//            key[(i<<3)|j] = ((s[i]>>j)&1);
-//        }
-//    }
-//    std::cout << key << std::endl;
-//    DESEncryption::Encrypt64(dst, src, key);
-//    return 0;
+    std::bitset<64> dst, src, key;
+    //std::string s = "OHAKONOH";
+    std::string s = "HONOKAHO";
+    for(int i = 0;i < 8;i++)
+    {
+        for(int j = 7;j >= 0;j--)
+        {
+            key[(i<<3)|(7-j)] = ((s[i]>>j)&1);
+        }
+    }
+    std::cout << "key:" << key << std::endl;
+    src = 999999999ll;
+    std::cout << "src:" << src << std::endl;
+    DESEncryption::Encrypt64(dst, src, key, false);
+    for(int i = 0;i < 16;i++)
+    {
+        std::cout << "key" << i << ":" << DESEncryption::keys[i] << std::endl;
+    }
+    std::cout << "=====" << std::endl;
+    std::cout << dst << std::endl;
+    src = dst;
+    DESEncryption::Encrypt64(dst, src, key, true);
+    std::cout << "=====" << std::endl;
+    std::cout << "dst:" << dst << std::endl;
 
-    std::cout << "Hello, World!" << std::endl;
+    /*std::bitset<8> b = 35;
+    std::cout << b << std::endl;
+    std::cout << ((b << 5) | (b >> 3)) << std::endl;*/
     return 0;
 }
